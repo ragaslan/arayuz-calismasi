af2d975c8b82e668d86f6ad5c48e77f8d4b15437
agaslan <agaslanramazan27@gmail.com>
2024-11-21 22:54:08 +0300

    footer bug is fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import Introduction from './components/Introduction'
 import ProductIntro from './components/ProductIntro'
 import WhyJoinUs from './components/WhyJoinUs'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 import Slider from './components/Slider'
 import BeforeFooter from './components/BeforeFooter'
 
